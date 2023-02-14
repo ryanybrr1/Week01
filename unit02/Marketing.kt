b7c8fa244e044312aa46f982e71b158b63f546f8
ryanybrr1 <118390554+ryanybrr1@users.noreply.github.com>
2023-02-14 12:06:45 -0600

    added Marketing.kt

---
+++ unit02/Marketing.kt
@@ -0,0 +1,18 @@
+/*
+* Ryan Ybarra
+* 02.3
+* Advertising Condidtional
+* 02/14/2023
+*/
+
+
+fun main(){
+    var friendsCount = 400
+
+    if(friendsCount>500){
+        println("Adopt a Dog Today!")
+    }
+    else{
+        println("Buy Cat Food At 20% Off")
+    }
+}
