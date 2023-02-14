abdcf29fb8791ecb9a80ac5c87aa5b9c22dafb82
ryanybrr1 <118390554+ryanybrr1@users.noreply.github.com>
2023-02-14 12:56:34 -0600

    Added Salary.kt

---
+++ unit02/Salary.kt
@@ -0,0 +1,14 @@
+/*
+* Ryan Ybarra
+*2.04 Name, Age, and Anaual Income
+* 02/14/2023
+*/ 
+
+fun main(){
+    var name = "Ryan Ybarra"
+    var age = 33
+    var annualPay = 150000.0
+
+    println("My name is $name, my age is $age and I hope to earn $$annualPay per year.")
+
+}
