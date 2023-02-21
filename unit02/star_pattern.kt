58052b02d023af2b3160c90e99b2986831196642
ryanybrr1 <118390554+ryanybrr1@users.noreply.github.com>
2023-02-21 10:25:22 -0600

    Add files via upload

---
+++ unit02/star_pattern.kt
@@ -0,0 +1,14 @@
+/* Ryan Ybarra
+*02/21/2023
+* 02.5 star pattern
+*/
+fun main(args: Array<String>) {
+
+    println("   *")
+    println("  ***")
+    println(" *****")
+    println("*******")
+    println(" *****")
+    println("  ***")
+    println("   *")
+}
