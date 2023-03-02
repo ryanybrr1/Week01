62aaf150ce97544b62b5337cc74384fbc7566cf6
ryanybrr1 <118390554+ryanybrr1@users.noreply.github.com>
2023-03-02 09:41:05 -0600

    Added BankCharges

---
+++ unit03/BankCharges.kt
@@ -0,0 +1,13 @@
+/*Ryan Ybarra
+* 03.8 Bank Charges
+* 03/01/2023*/
+
+fun main(){
+
+    print("Enter number of checks written for the month: ");
+    var checks = readln()
+    var c = checks.toDouble()
+
+    val BASE = 10.0
+
+    var total: Double
