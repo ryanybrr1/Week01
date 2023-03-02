102e4a35d83159d2bf5cdb7d024cd4e0791cbbbd
ryanybrr1 <118390554+ryanybrr1@users.noreply.github.com>
2023-03-02 09:45:49 -0600

    Added massWeight

--- unit03/massWeight.kt
+++ unit03/massWeight.kt
@@ -1,1 +1,25 @@
+/*Ryan Ybarra
+* 03.4 Mass and Weight
+* 03/01/2023*/
 
+fun main(){
+
+    println("Enter an object's mass: ")
+    var mass = readLine()!!
+    var m = mass.toDouble()
+
+    var weight = m * 9.8
+    var w = String.format("%.2f", weight)
+
+    println("The object's weight in Newtons is $w")
+
+
+    if(weight > 1000){
+        println("The object is too heavy!")
+    }
+    else if(weight < 10){
+        println("The object is too light!")
+    }
+    else{
+        println("Your object is perfect!")
+    }
